45d97daf1293fa678392d77c3ebd5d27db53b83f
ALeX <alexdeoliveira.poli@gmail.com>
2021-05-08 13:48:20 -0300

    End of iExpense SwiftUI Tutorial 7 of 11 - Building a list we can delete from

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,10 +7,40 @@
 
 import SwiftUI
 
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                                        self.expenses.items.append(expense)
+                                    }) {
+                                        Image(systemName: "plus")
+                                    })
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
